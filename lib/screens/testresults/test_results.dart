038ed0f1e6ab477e42f7696087b977e29a65d274
Hajra <hshahzad.bscs20seecs@seecs.edu.pk>
2022-09-15 10:37:18 +0500

    screen changes

--- lib/screens/testresults/test_results.dart
+++ lib/screens/testresults/test_results.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:omnihealth/constants/constants.dart';
 import 'package:omnihealth/Components/lab_result_holder.dart';
 
+
 class TestResults extends StatefulWidget {
   const TestResults({Key? key}) : super(key: key);
 
@@ -19,6 +20,7 @@ class _TestResultsState extends State<TestResults> {
     'Item 5',
   ];
   String dropdownvalue = 'Filter by year';
+
   int index = 1;
   @override
   Widget build(BuildContext context) {
@@ -38,7 +40,7 @@ class _TestResultsState extends State<TestResults> {
             children: [
               Container(
                 width: double.infinity,
-                margin: const EdgeInsets.fromLTRB(20, 25, 20, 10),
+                margin: const EdgeInsets.fromLTRB(20, 25, 20, 5),
                 padding:
                     const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
                 decoration: const BoxDecoration(
@@ -55,50 +57,16 @@ class _TestResultsState extends State<TestResults> {
                 ),
               ),
               Expanded(
-                child: ListView(
+                child: SingleChildScrollView(
                   scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.all(8),
-                  children: [
-                    SizedBox(
-                      width: 10,
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      child: LabResultHolder(
-                          heading: 'Heading',
-                          date: '2nd September 2022',
-                          labName: 'Hospital and Lab Name'),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    TextButton(
-                      onPressed: (){},
-                      child: LabResultHolder(
-                          heading: 'Heading',
-                          date: '2nd September 2022',
-                          labName: 'Hospital and Lab Name'),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    TextButton(
-
-                      onPressed: (){},
-                      child: LabResultHolder(
-                          heading: 'Heading',
-                          date: '2nd September 2022',
-                          labName: 'Hospital and Lab Name'),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                  ],
+                  child: Row(
+                    children: resultInitializer(),
+                  ),
                 ),
               ),
               Container(
                 width: double.infinity,
-                margin: const EdgeInsets.fromLTRB(20, 25, 20, 10),
+                margin: const EdgeInsets.fromLTRB(20, 5, 20, 10),
                 padding:
                     const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
                 decoration: const BoxDecoration(
@@ -116,7 +84,7 @@ class _TestResultsState extends State<TestResults> {
               ),
               Expanded(
                 child: ListView(
-                  children: [
+                  /*children: [
                     Padding(
                       padding: const EdgeInsets.fromLTRB(20, 0, 0, 10),
                       child: Text(
@@ -126,56 +94,67 @@ class _TestResultsState extends State<TestResults> {
                     ),
                     SingleChildScrollView(
                       scrollDirection: Axis.horizontal,
-                      child: Row(
-                        children: [
-                          SizedBox(
-                            width: 10,
-                          ),
-                          TextButton(
-
-                            onPressed: (){},
-                            child: LabResultHolder(
-                                heading: 'Heading',
-                                date: '2nd September 2022',
-                                labName: 'Hospital and Lab Name'),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          TextButton(
-
-                            onPressed: (){},
-                            child: LabResultHolder(
-                                heading: 'Heading',
-                                date: '2nd September 2022',
-                                labName: 'Hospital and Lab Name'),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          TextButton(
-
-                            onPressed: (){},
-                            child: LabResultHolder(
-                                heading: 'Heading',
-                                date: '2nd September 2022',
-                                labName: 'Hospital and Lab Name'),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          TextButton(
-
-                            onPressed: (){},
-                            child: LabResultHolder(
-                                heading: 'Heading',
-                                date: '2nd September 2022',
-                                labName: 'Hospital and Lab Name'),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                        ],
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              width: 10,
+                            ),
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                padding: EdgeInsets.zero,
+                              ),
+                              onPressed: (){},
+                              child: LabResultHolder(
+                                  heading: 'Heading',
+                                  date: '2nd September 2022',
+                                  labName: 'Hospital and Lab Name'),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                padding: EdgeInsets.zero,
+                              ),
+                              onPressed: (){},
+                              child: LabResultHolder(
+                                  heading: 'Heading',
+                                  date: '2nd September 2022',
+                                  labName: 'Hospital and Lab Name'),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                padding: EdgeInsets.zero,
+                              ),
+                              onPressed: (){},
+                              child: LabResultHolder(
+                                  heading: 'Heading',
+                                  date: '2nd September 2022',
+                                  labName: 'Hospital and Lab Name'),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                padding: EdgeInsets.zero,
+                              ),
+                              onPressed: (){},
+                              child: LabResultHolder(
+                                  heading: 'Heading',
+                                  date: '2nd September 2022',
+                                  labName: 'Hospital and Lab Name'),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                     Padding(
@@ -187,59 +166,71 @@ class _TestResultsState extends State<TestResults> {
                     ),
                     SingleChildScrollView(
                       scrollDirection: Axis.horizontal,
-                      child: Row(
-                        children: [
-                          SizedBox(
-                            width: 10,
-                          ),
-                          TextButton(
-
-                            onPressed: (){},
-                            child: LabResultHolder(
-                                heading: 'Heading',
-                                date: '2nd September 2022',
-                                labName: 'Hospital and Lab Name'),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          TextButton(
-
-                            onPressed: (){},
-                            child: LabResultHolder(
-                                heading: 'Heading',
-                                date: '2nd September 2022',
-                                labName: 'Hospital and Lab Name'),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          TextButton(
-
-                            onPressed: (){},
-                            child: LabResultHolder(
-                                heading: 'Heading',
-                                date: '2nd September 2022',
-                                labName: 'Hospital and Lab Name'),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          TextButton(
-
-                            onPressed: (){},
-                            child: LabResultHolder(
-                                heading: 'Heading',
-                                date: '2nd September 2022',
-                                labName: 'Hospital and Lab Name'),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                        ],
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              width: 10,
+                            ),
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                padding: EdgeInsets.zero,
+                              ),
+                              onPressed: (){},
+                              child: LabResultHolder(
+                                  heading: 'Heading',
+                                  date: '2nd September 2022',
+                                  labName: 'Hospital and Lab Name'),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                padding: EdgeInsets.zero,
+                              ),
+                              onPressed: (){},
+                              child: LabResultHolder(
+                                  heading: 'Heading',
+                                  date: '2nd September 2022',
+                                  labName: 'Hospital and Lab Name'),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                padding: EdgeInsets.zero,
+                              ),
+                              onPressed: (){},
+                              child: LabResultHolder(
+                                  heading: 'Heading',
+                                  date: '2nd September 2022',
+                                  labName: 'Hospital and Lab Name'),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                padding: EdgeInsets.zero,
+                              ),
+                              onPressed: (){},
+                              child: LabResultHolder(
+                                  heading: 'Heading',
+                                  date: '2nd September 2022',
+                                  labName: 'Hospital and Lab Name'),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                          ],
+                        ),
                       ),
                     )
-                  ],
+                  ],*/
+                  children: yearResultGenerator(),
                 ),
               ),
               Container(
@@ -297,3 +288,75 @@ class _TestResultsState extends State<TestResults> {
   }
 }
 
+//TODO: Replace this with dynamic data from the database
+List<Widget> resultInitializer (){
+  List <Widget> resultHolderList = [
+    SizedBox(width: 15,),
+  ];
+  String heading = 'Heading';
+  String date = '3rd September 2022';
+  String labName = 'Lab Name';
+  for (var i =0; i < 3; i++){ //todo: change index to the number of results in the month
+    resultHolderList.add(
+      TextButton(
+        onPressed: (){},
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 5.0),
+        ),
+        child: LabResultHolder(heading: heading, date: date, labName: labName,
+        ),
+      ),
+    );
+  }
+  return resultHolderList;
+}
+//TODO: get results from database within the time limit of an year
+//TODO: Sort them into different lists according to month and display these lists on screen
+List <Widget> yearResultGenerator(){
+  List <Widget> resultList = [
+
+  ];
+  DateTime nowDate = DateTime.now();
+  int month = nowDate.month;
+  String monthName = '';
+  switch (month) {
+    case 1: monthName = 'JAN'; break;
+    case 2: monthName = 'FEB'; break;
+    case 3: monthName = 'MAR'; break;
+    case 4: monthName = 'APR'; break;
+    case 5: monthName = 'MAY'; break;
+    case 6: monthName = 'JUN'; break;
+    case 7: monthName = 'JUL'; break;
+    case 8: monthName = 'AUG'; break;
+    case 9: monthName = 'SEP'; break;
+    case 10: monthName = 'OCT'; break;
+    case 11: monthName = 'NOV'; break;
+    case 12: monthName = 'DEC'; break;
+  }
+  for (var i = 0; i < 1; i++){ //todo:change outer loop index to the number of months
+    resultList.add(
+      Padding(
+        padding: const EdgeInsets.fromLTRB(20, 0, 0, 10),
+        child: Text(
+          monthName,
+          style: kMiniGreyText,
+        ),
+      ),
+    );
+    for (var j = 0; j < 1; j++){//inner loop only runs once
+      resultList.add(
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: resultInitializer(),
+              ),
+            ),
+          )
+      );
+    }
+  }
+
+  return resultList;
+}
